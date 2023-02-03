f588ddad32a71ff2ef33f7c7c91f5e763a6b8b5b
arantzalemasurier <arantzadba@gmail.com>
2023-02-03 11:35:08 +0100

    Mejora componente LocalInfo

--- src/components/LocalInfo.tsx
+++ src/components/LocalInfo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Typography from '@material-ui/core/Typography';
 import styled from 'styled-components';
 import { CardMedia } from '@material-ui/core';
@@ -14,24 +14,26 @@ interface Props {
 
 const StyledCardMedia = styled(CardMedia)`
   text-align: center;
-  margin-top: 7%;
-  margin-bottom: 7%;
+  margin: 7% auto;
 `;
 
-const LocalInfo: React.FC<Props> = ({ today: { city, country, date, population } }) => {
-  return (
-    <StyledCardMedia>
-      <Typography variant="h3" gutterBottom>
-        {city}, {country}
-      </Typography>
-      <Typography variant="h5" gutterBottom>
-        {date}
-      </Typography>
-      <Typography variant="h6" gutterBottom>
-        Población: {population.toLocaleString()}
-      </Typography>
-    </StyledCardMedia>
-  );
-};
+class LocalInfo extends Component<Props> {
+  render() {
+    const { city, country, date, population } = this.props.today;
+    return (
+      <StyledCardMedia>
+        <Typography variant="h3" gutterBottom>
+          {city}, {country}
+        </Typography>
+        <Typography variant="h5" gutterBottom>
+          {date}
+        </Typography>
+        <Typography variant="h6" gutterBottom>
+          Población: {population.toLocaleString()}
+        </Typography>
+      </StyledCardMedia>
+    );
+  }
+}
 
 export default LocalInfo;
