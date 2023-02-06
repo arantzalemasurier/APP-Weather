ffd7786f3063e18ad5c683a8431e84d1a2ac97f2
arantzalemasurier <arantzadba@gmail.com>
2023-02-06 11:49:27 +0100

    Search en tres partes

---
+++ src/components/SearchContainer.tsx
@@ -0,0 +1,12 @@
+// components/SearchContainer.tsx
+import styled from 'styled-components';
+
+const SearchContainer = styled.form`
+  padding-top: 64px;
+  width: 400px;
+  margin: 0 auto;
+  background-color: transparent;
+  font-family: 'Raleway', sans-serif;
+`;
+
+export default SearchContainer;
